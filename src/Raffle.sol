b320102f0544e287167cfe2a0e42b89692632a0e
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2024-06-05 16:30:16 +0200

    Added entranceFee variable and custom error for not sending enough Eth
    Create an immutable variable for saving the entranceFee value to
    participate in the raffle and added the constructor for initialising it.
    Also created a getter function for the entranceFee variable and added a
    custom error for checking that enough Eth is sent for covering the
    entranceFee when calling enterRaffle()

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,12 +2,31 @@
 
 pragma solidity ^0.8.18;
 
+error Raffle_NotEnoughEthSent();
+
 /**@title A sample Raffle Contract
  * @author Patrick Collins
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
-
 contract Raffle {
+    uint256 private immutable i_entranceFee;
 
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    /** External Functions */
+    function getentranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    /** Public Functions */
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughEthSent();
+        }
+    }
+
+    function pickWinner() public {}
 }
