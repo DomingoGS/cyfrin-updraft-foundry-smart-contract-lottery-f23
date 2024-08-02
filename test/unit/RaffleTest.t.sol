d0bed89446b594b4af109de0a63c1072bf7eb681
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2024-08-02 17:16:14 +0200

    Add an event test
    Added the necessary code for using the expectEmit() function to test and
    confirm that the EnteredRaffle event is emited when a player enteres the
    raffle.

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -21,6 +21,8 @@ contract RaffleTest is Test {
     address public PLAYER = makeAddr("player");
     uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
 
+    event EnteredRaffle(address indexed player); // copied from Raffle.sol for event testing to work
+
     function setUp() external {
         DeployRaffle deployer = new DeployRaffle();
         (raffle, helperConfig) = deployer.deployContract();
@@ -56,5 +58,15 @@ contract RaffleTest is Test {
         // Assert
         address playerRecorded = raffle.getPlayer(0);
         assert(playerRecorded == PLAYER);
+    }
+
+    function testEnteringRaffleEmitsEvent() public {
+        // Arrange
+        vm.prank(PLAYER);
+        // Act
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit EnteredRaffle(PLAYER);
+        // Assert
+        raffle.enterRaffle{value: entranceFee}();
     }
 }
